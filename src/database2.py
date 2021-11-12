c6c1af44bd7b3150fe811f6ce92d4d2a6b1226ca
jonathan <jonathan.chong@xendity.com>
2021-11-12 23:59:43 +0800

    change postgresql to postgres for heroku

--- src/database2.py
+++ src/database2.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgres import UUID
 import uuid
 
 import string
