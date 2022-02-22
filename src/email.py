4e823e23b3401710121f01dbd02aa50bab2127f5
jonathan <jonathan.chong@xendity.com>
2022-02-22 15:49:49 +0800

    upload release 1

--- src/email.py
+++ src/email.py
@@ -8,10 +8,14 @@ mail = Mail()
 
 
 def send_email(to, subject, template):
-    msg = Message(
-        subject,
-        recipients=[to],
-        html=template,
-        sender=('LCMS Balakong Unity Church', 'fyneos88@gmail.com')
-    )
-    mail.send(msg)
+    try:
+        msg = Message(
+            subject,
+            recipients=[to],
+            html=template,
+            sender=('LCMS Balakong Unity Church', 'fyneos88@gmail.com')
+        )
+        mail.send(msg)
+        return True
+    except:
+        return False
